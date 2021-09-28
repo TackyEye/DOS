e92bab9ce73b6a89a8000fc5bccf04a7becaad4f
TackyEye <observantmagic@outlook.com>
2021-09-28 21:18:52 +0530

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,14 +8,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          child: Image(
+        body: Center(
+          child: Stack( 
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+            child: Image(
             image: AssetImage('assets/poster.jpg'),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            //fit: BoxFit.cover,
           ),
-        ) 
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width/2,
+            height: MediaQuery.of(context).size.height/2,
+                alignment: Alignment.bottomCenter,
+                child: TextButton(
+                  child: Text('Tap to Play'),
+    style: TextButton.styleFrom(
+      primary: Colors.white,
+      backgroundColor: Colors.teal,
+      onSurface: Colors.grey,
+    ),
+    onPressed: () {
+      print('Pressed');
+    },
+                ),                
+              )
+            ]
+        ), 
+    )
     );
   }
 }
